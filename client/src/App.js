37c2f0fd2fbcda35df81cdf89773aea01ebecc90
thewildpenguin <gaetan.puleo.dev@gmail.com>
2021-06-27 10:24:54 +0200

    chore: small changes and a not funny joke

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
 import styles from './App.module.scss';
 import Main from './controllers/Main';
 
+// TO THE MOON!! 
+// (Bitcoin, the Apollo mission, do you get it??) 
+// sorry I won't do it again...
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   uri: 'http://localhost:9000/graphql',
